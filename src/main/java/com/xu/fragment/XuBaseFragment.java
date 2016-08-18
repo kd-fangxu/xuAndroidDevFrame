6d39fb2efece99f9ea8c8c17f6ff87139fa5aa02
fangxu <gfx123321@126.com>
2016-08-18 14:46:17 +0800

    初版节点

--- src/main/java/com/xu/fragment/XuBaseFragment.java
+++ src/main/java/com/xu/fragment/XuBaseFragment.java
@@ -6,13 +6,23 @@ import android.support.v4.app.Fragment;
  * Created by developer on 16/8/2.
  */
 public class XuBaseFragment extends Fragment {
+    public boolean isShownInAct() {
+        return isShownInAct;
+    }
+
+    public void setShownInAct(boolean shownInAct) {
+        isShownInAct = shownInAct;
+    }
+
+    boolean isShownInAct = true;//是否在act中显示
+
     public XuBaseFragment() {
     }
+
     ;
 
-
-    public static XuBaseFragment newInstance(){
-        XuBaseFragment fragment=new XuBaseFragment();
+    public static XuBaseFragment newInstance() {
+        XuBaseFragment fragment = new XuBaseFragment();
         return fragment;
     }
 }
