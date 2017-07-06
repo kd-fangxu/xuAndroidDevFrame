52aa6dd5cc64966823e7ac29e45f22aac806d2a1
fangxu <gfx123321@126.com>
2017-07-06 14:29:24 +0800

    sync

--- src/main/java/com/xudev/utils/GsonFormatUtil.java
+++ src/main/java/com/xudev/utils/GsonFormatUtil.java
@@ -5,10 +5,8 @@ import com.google.gson.reflect.TypeToken;
 
 public class GsonFormatUtil<T> {
 
-	public  T formatJs(String json) {
-		Gson gson = new Gson();
-		T t = gson.fromJson(json, new TypeToken<T>() {
-		}.getType());
-		return t;
-	}
+    public T formatJs(String json) {
+        return new Gson().fromJson(json, new TypeToken<T>() {
+        }.getType());
+    }
 }
