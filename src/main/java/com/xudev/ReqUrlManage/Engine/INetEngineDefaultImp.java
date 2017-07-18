40a744a501906407e68497bd04a6b55b25a90bb6
XU\xu <gfx123321@126.com>
2017-07-18 16:37:18 +0800

    sync

--- src/main/java/com/xudev/ReqUrlManage/Engine/INetEngineDefaultImp.java
+++ src/main/java/com/xudev/ReqUrlManage/Engine/INetEngineDefaultImp.java
@@ -84,7 +84,7 @@ public class INetEngineDefaultImp implements INetEngine {
                     public void onError(Throwable ex, boolean isOnCallback) {
                         L.e(reqParam.toString() + ex.toString());
                         if (commonBusListener != null) {
-                            commonBusListener.onFailed("网络请求失败");
+                            commonBusListener.onFailed("网络请求失败或回调方法中出现异常");
                         }
 
                     }
